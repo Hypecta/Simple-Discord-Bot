b478ada42df5e6a9bd7d61e8cda04d53af7bdbaa
kohjie <1802967@sit.singaporetech.edu.sg>
2023-04-10 16:03:39 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Simple Discord Bot
- A simple Discord bot to react to messages
+# Simple Discord Bot :D
+
+ A simple discord bot to react to stinky messages!!
+
+
+## Silly Features :P
+
+- simple regex matching on new messages
+- good/bad bot
+- reacting when @'d
+
+## Remember to do this!
+
+The `requirements.txt` file lists the Python libraries required to run this discord bot:
+```bash
+pip install -r requirements.txt
+```
+
+To run this project, you will need to add the `BOT_TOKEN` to your .env file
+
+In case you are unable to find the `BOT_TOKEN`, follow [this guide](https://docs.discordbotstudio.org/setting-up-dbs/finding-your-bot-token)!
+
+
+### Look at my bot go!
+![](https://cdn.discordapp.com/attachments/1094887411986739270/1094889738340683857/image.png)
---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord, string, re, os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+"""
+bad_words consist of the words that are prohibited
+- mommy
+"""
+bad_words = [r"\bm{1,}[ou0]{1,}mm{1,}y\b"]
+
+# Import Environment Variables
+load_dotenv()
+
+# Initialize Bot Intents & Permissions
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+bot = commands.Bot(command_prefix="", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is online.')
+
+@bot.event
+async def on_message(message):
+    # Skip if new message from bot
+    if message.author.bot:
+        return
+    
+    # Match new message from bad_words list
+    if re.match(pattern="(" + ")|(".join(bad_words) + ")", string=message.content.lower().translate({ord(c): None for c in string.whitespace}), flags=re.IGNORECASE):
+        await message.channel.send(f'{message.author.mention} FUCK YOU DEGENERATE SCUM. YOU WASTE OF GOVERNMENT TAX PAYER MONEY. FORTNIGHTLY PAYMENT PIECE OF SHIT GO AND MAKE SOME CONTRIBUTIONS TO SOCIETY.')
+
+    # good/bad bot prompts
+    if "good bot" in message.content.lower():
+        await message.channel.send(f'<a:peepoblushshake:1050640802533093416>')
+
+    if "bad bot" in message.content.lower():
+        await message.channel.send(f'<a:Blubbers:1000378228483031162>')
+    
+    # When bot is @'d
+    if bot.user.mentioned_in(message):
+        await message.add_reaction(f'<:reallyinnocent:1094866016691036260>')
+
+bot.run(os.getenv('BOT_TOKEN'))
